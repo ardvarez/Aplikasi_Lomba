7c557af61d45a122d94a35c8edebbbc9affd51b0
rhasnafadhila <radenhasna.fadhila@gmail.com>
2019-03-31 14:09:32 +0700

    daftar tim front end

---
+++ tambahtim.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <form style="margin: 100px 20%;">
+      <h1>Daftarkan TIM</h1>
+      <br>  
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Nama Tim</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Masukan nama tim">
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Nama Ketua</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Masukan nama ketua">
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Nama Anggota 1</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Masukan nama anggota 1">
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Nama Anggota 2</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Masukan nama anggota 2">
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Nama Anggota 3</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Masukan nama anggota 3">
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Nama Anggota 4</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Masukan nama anggota 4">
+      </div>
+
+      <button class="btn btn-primary">  Masukan Daftar </button>
+
+    </form>
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="jquery-2.1.1.js"></script>
+
+ 
+</body>
+</html>
+
