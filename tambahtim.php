9d90057b9aa0723950552cd6434642a6589505e5
rhasnafadhila <radenhasna.fadhila@gmail.com>
2019-04-02 12:52:39 +0700

    fitur tambah tim

--- tambahtim.php
+++ tambahtim.php
@@ -5,19 +5,16 @@ $conn = mysqli_connect("localhost","root","","aplikasi_lomba");
 
 //apakah tombol subit sudah
 if(isset($_POST["submit"])){
-       require 'function tambah tim.php';
+ require 'function_tambah_tim.php';
+
+ if (tambah($_POST)>0) {
+   echo "<h1> sukses</h1>";
+ }else{
+  echo "gagal!";
+ 
 
 
-
-
-       if (tambah($_POST)>0) {
-             echo "sukses";
-      }else{
-            echo "gagal!";
-            echo_mysql_eror($conn); 
-
-
-     }
+}
 }
 //cek apakah data berhasil ditambahkan atau tidak
 ?>
@@ -28,54 +25,50 @@ if(isset($_POST["submit"])){
 <head>
   <title></title>
 
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
 </head>
 <body>
 
-    <form style="margin: 100px 20%;">
-      <h1>Daftarkan TIM</h1>
-      <br>  
-      <div class="form-group">
-        <label for="nama_tim">Nama Tim</label>
-        <input type="nama_tim" class="form-control" name="nama_tim" id="nama_tim" placeholder="Masukan nama tim">
-      </div>
-      <div class="form-group">
-        <label for="nama_ketua">Nama Ketua</label>
-        <input type="nama_ketua" class="form-control" name="nama_ketua" id="nama_ketua" placeholder="Masukan nama ketua">
-      </div>
-  
-      <div class="form-group">
-        <label for="anggota1">Nama Anggota 1</label>
-        <input type="text" class="form-control" name="anggota1" id="anggota1" placeholder="Masukan nama anggota 1">
-      </div>
-      <div class="form-group">
-        <label for="anggota2">Nama Anggota 2</label>
-        <input type="text" class="form-control" name="anggota2" id="anggota2" placeholder="Masukan nama anggota 2">
-      </div>
-        <div class="form-group">
-        <label for="anggota3">Nama Anggota 3</label>
-        <input type="text" class="form-control" name="anggota3" id="anggota3" placeholder="Masukan nama anggota 3">
+  <form style="margin: 100px 20%;" method="POST" action="">
+    <h1>Daftarkan TIM</h1>
+    <br>  
+    <div class="form-group">
+      <label for="nama_tim">Nama Tim</label>
+      <input type="text" class="form-control" name="nama_tim" id="nama_tim" placeholder="Masukan nama tim">
+    </div>
+    <div class="form-group">
+      <label for="nama_ketua">Nama Ketua</label>
+      <input type="text" class="form-control" name="nama_ketua" id="nama_ketua" placeholder="Masukan nama ketua">
+    </div>
 
-      </div>
-      <div class="form-group">
-        <label for="angota4">Nama Anggota 4</label>
-        <input type="text" class="form-control" name="anggota4" id="anggota4" placeholder="Masukan nama anggota 4">
-     
-      </div>
+    <div class="form-group">
+      <label for="anggota1">Nama Anggota 1</label>
+      <input type="text" class="form-control" name="anggota1" id="anggota1" placeholder="Masukan nama anggota 1">
+    </div>
+    <div class="form-group">
+      <label for="anggota2">Nama Anggota 2</label>
+      <input type="text" class="form-control" name="anggota2" id="anggota2" placeholder="Masukan nama anggota 2">
+    </div>
+    <div class="form-group">
+      <label for="anggota3">Nama Anggota 3</label>
+      <input type="text" class="form-control" name="anggota3" id="anggota3" placeholder="Masukan nama anggota 3">
 
-      <div class="btn btn-primary">
-         <button name="submit"> Masukkan daftar</button>
+    </div>
+    <div class="form-group">
+      <label for="angota4">Nama Anggota 4</label>
+      <input type="text" class="form-control" name="anggota4" id="anggota4" placeholder="Masukan nama anggota 4">
 
-        
-      </div>
+    </div>
 
-      <!-- <button class="btn btn-primary"name="submit" type="submit">  Masukan Daftar </button> -->
+     <button class="btn btn-primary" name="submit">  Masukan Daftar </button>
 
-    </form>
+   <!-- <button class="btn btn-primary"name="submit" type="submit">  Masukan Daftar </button> -->
 
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="jquery-2.1.1.js"></script>
+ </form>
+
+ <script type="text/javascript" src="js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="jquery-2.1.1.js"></script>
 
  
 </body>
